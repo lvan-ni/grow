7f0f061a784b812e97341128ef326a1f157ffb3f
gnilvan <gnilvan@users.noreply.github.com>
2023-08-13 17:07:39 +0200

    start backend deployment

--- backend/server.ts
+++ backend/server.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { connectDB, getPlants, createNewPlant, updatePlant, deletePlant } from './db';
 
 const app = express();
-const PORT = 3001;
+const PORT = 3001 || process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
